62525fe8d4ff25180f4d8abb9bacd42363c0299b
greenkoopa <green.koopa667@gmail.com>
2017-08-17 11:09:55 -0400

    Started using jsx. Renamed

---
+++ src/components/Display.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import WebM from './WebM'
+
+class Display extends Component {
+  render() {
+    return (
+      <div className="Display">
+        <WebM></WebM>
+      </div>
+    );
+  }
+}
+
+export default Display;
