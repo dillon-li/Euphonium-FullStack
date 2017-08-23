4a0bff90b903d8659497910967995d33476a72bf
greenkoopa <green.koopa667@gmail.com>
2017-08-23 10:44:23 -0400

    Added basic column layout

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap'
+import { Button, Col, Row, Container } from 'reactstrap';
 
 import WebM from './WebM'
 
@@ -10,7 +10,8 @@ class Display extends Component {
     this.state = {
       works: [],
       webfile: 'someString',
-      buttonText: 'Begin Your Journey'
+      buttonText: 'Begin Your Journey',
+      displaying: false
     }
   }
 
@@ -26,27 +27,56 @@ class Display extends Component {
     console.log(webfiles.length)
     const item = webfiles[Math.floor(Math.random()*webfiles.length)];
     console.log(item.toString())
-    const urlString = "../../images/webms/" + item.toString()
+    const encoded = encodeURIComponent(item.toString())
+    const urlString = "../../images/webms/" + encoded
     this.setState (
       {
         webfile: urlString,
-        buttonText: 'More Memes'
+        buttonText: 'More Memes',
+        displaying: true
       }
     );
   }
 
   render() {
     console.log(this.state)
+    const displaying = this.state.displaying
+    if (displaying) {
     return (
-      <div className="Display">
-      <Button
-        onClick={this.displayMemes.bind(this)}
-        color="primary">
-        {this.state.buttonText}
-      </Button>
+      <div class="display">
+      <Container fluid className="Display">
+        <Button
+          onClick={this.displayMemes.bind(this)}
+          color="primary">
+          {this.state.buttonText}
+        </Button>
+        <Row>
+        <Col xs="8">
         <WebM url={this.state.webfile}></WebM>
+        </Col>
+        <Col xs="1">
+          <Button color="danger" block>DANK</Button>
+          <Button color="primary" block>Not Dank</Button>
+        </Col>
+        </Row>
+      </Container>
       </div>
-    );
+      );
+    }
+    else {
+      return (
+        <div class="display">
+        <Container fluid>
+          <Button
+            onClick={this.displayMemes.bind(this)}
+            color="primary">
+            {this.state.buttonText}
+          </Button>
+          <WebM url={this.state.webfile}></WebM>
+        </Container>
+        </div>
+      );
+    }
   }
 }
 
